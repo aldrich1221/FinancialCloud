cc7addb2fcf190672cf106ab1adf17727c48e676
chendavid <David.Chen@garmin.com>
2024-08-05 13:45:23 +0800

    modify default zone

---
+++ FinancialCloud/BasicAssetAllocation/src/main/java/com/finance/basicassetallocation/BasicAssetAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.finance.basicassetallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAssetAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAssetAllocationApplication.class, args);
+	}
+
+}
---
+++ FinancialCloud/BasicAssetAllocation/src/test/java/com/finance/basicassetallocation/BasicAssetAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.basicassetallocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAssetAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
