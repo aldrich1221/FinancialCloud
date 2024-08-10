23d8f226536a4ff5341c57af7aeda321cb070822
User <aldrich1221@gmail.com>
2024-08-11 00:23:43 +0800

    grpc

---
+++ FinancialCloud/BasicAssetAllocation/src/main/java/com/finance/basicassetallocation/services/GrpcClientDailyStockPriceService.java
@@ -0,0 +1,7 @@
+package com.finance.basicassetallocation.services;
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import com.example.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
