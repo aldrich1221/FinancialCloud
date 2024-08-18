9361902b14b917f61d2127d0edc574e4f03a36d7
Chen, David <david.chen@garmin.com>
2024-08-18 16:55:40 +0800

    add grpc interface

---
+++ FinancialCloud/GRPCInterface/src/main/java/com/finance/grpcinterface/GrpcInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.finance.grpcinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcInterfaceApplication.class, args);
+    }
+
+}
---
+++ FinancialCloud/GRPCInterface/src/test/java/com/finance/grpcinterface/GrpcInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.grpcinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
