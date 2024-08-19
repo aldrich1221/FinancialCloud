3bc9bb5c5f0b632b0252668ed134749868fc81cb
Chen, David <david.chen@garmin.com>
2024-08-19 14:12:17 +0800

    modify finance data proto

--- FinancialCloud/DailyStockDataMicroservice/src/main/java/com/finance/dailystockdatamicroservice/DailyStockDataMicroServiceApplication.java
+++ FinancialCloud/DailyStockDataMicroservice/src/main/java/com/finance/dailystockdatamicroservice/DailyStockDataMicroServiceApplication.java
@@ -1,13 +1,17 @@
 package com.finance.dailystockdatamicroservice;
 
+import com.finance.dailystockdatamicroservice.services.grpc.HelloWorldServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DailyStockDataMicroServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DailyStockDataMicroServiceApplication.class, args);
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        SpringApplication.run(DailyStockDataMicroServiceApplication.class, args);
+        HelloWorldServer.main(args);
     }
 
 }
